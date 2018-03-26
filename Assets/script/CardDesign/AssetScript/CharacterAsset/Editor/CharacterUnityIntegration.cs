338196249780ac98f47ebd49c43954187ae267a1
SagunMan <sanmantam@gmail.com>
2018-03-26 21:27:56 +0545

    character asset added

---
+++ Assets/script/CardDesign/AssetScript/CharacterAsset/Editor/CharacterUnityIntegration.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+static class CharacterUnityIntegration {
+
+	[MenuItem("Assets/Create/CharacterAsset")]
+	public static void CreateYourScriptableObject() {
+        ScriptableObjectUtitity2.CreateAsset<CharacterAsset>();
+	}
+
+}
