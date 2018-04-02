339875c3ae8c42df820c8f9d6c8b4e1c66697a6f
SagunMan <sanmantam@gmail.com>
2018-04-02 23:42:01 +0545

    initial commit

---
+++ Assets/script/AssetScript/CardAsset/CardAsset.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum TargetingOptions
+{
+    NoTarget,
+    AllCards,
+    EnemyCards,
+    YourCards,
+    AllCharacters,
+    EnemyCharacters,
+    YourCharacters,
+    AllCreatures,
+    EnemyCreatures,
+    YourCreatures
+}
+
+public class CardAsset: ScriptableObject
+{
+
+    [Header("General info")]
+    [TextArea(2, 3)]
+    public string Description;
+    public Sprite CardImage;
+    public Sprite CardBodyImage;
+    public Sprite CardHeaderImage;
+    public Sprite CardDescriptionImage;
+    public int ResourceCost;
+
+    [Header("Unit info")]
+    public int MaxHealth;
+    public int Attack;
+    public int AttacksForOneTurn = 1;
+    public bool Charge;
+    public string UnitScriptName;
+
+    [Header("Spell info")]
+    public string SpellScriptName;
+    public int specialUnitAmount;
+    public int specialSpellAmount;
+    public TargetingOptions Targets;
+
+}
+
---
+++ Assets/script/Logic/CardLogic.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[System.Serializable]
+public class CardLogic: IIdentifiable
+{
+    public Player owner;
+    public int UniqueCardID; 
+
+    public CardAsset ca;
+    public GameObject VisualRepresentation;
+
+    private int baseResourceCost;
+    public SpellEffect effect;
+
+    public int ID
+    {
+        get{ return UniqueCardID; }
+    }
+
+    public int CurrentResourceCost{ get; set; }
+
+    public bool CanBePlayed
+    {
+        get
+        {
+            bool ownersTurn = (TurnManager.Instance.whoseTurn == owner);
+            // for spells the amount of characters on the field does not matter
+            bool fieldNotFull = true;
+            // but if this is a creature, we have to check if there is room on board (table)
+            if (ca.MaxHealth > 0)
+                fieldNotFull = (owner.table.CreaturesOnTable.Count < 7);
+            //Debug.Log("Card: " + ca.name + " has params: ownersTurn=" + ownersTurn + "fieldNotFull=" + fieldNotFull + " hasMana=" + (CurrentManaCost <= owner.ManaLeft));
+            return ownersTurn && fieldNotFull && (CurrentResourceCost <= owner.ManaLeft);
+        }
+    }
+
+    public CardLogic(CardAsset ca)
+    {
+        this.ca = ca;
+        UniqueCardID = IDFactory.GetUniqueID();
+        //UniqueCardID = IDFactory.GetUniqueID();
+        baseResourceCost = ca.ResourceCost;
+        ResetManaCost();
+        if (ca.SpellScriptName!= null && ca.SpellScriptName!= "")
+        {
+            effect = System.Activator.CreateInstance(System.Type.GetType(ca.SpellScriptName)) as SpellEffect;
+        }
+        CardsCreatedThisGame.Add(UniqueCardID, this);
+    }
+
+    public void ResetManaCost()
+    {
+        CurrentResourceCost = baseResourceCost;
+    }
+
+    // STATIC (for managing IDs)
+    public static Dictionary<int, CardLogic> CardsCreatedThisGame = new Dictionary<int, CardLogic>();
+
+}
